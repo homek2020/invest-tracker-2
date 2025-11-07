199b5f2e5b9bfdad0d221bbc3fbae7493648527c
homek2020 <60591846+homek2020@users.noreply.github.com>
2025-11-08 02:11:31 +0300

    Fix Fastify JWT user typing

--- backend/src/types/jwt.d.ts
+++ backend/src/types/jwt.d.ts
@@ -1,7 +1,7 @@
-import 'fastify';
+import '@fastify/jwt';
 
-declare module 'fastify' {
-  interface FastifyRequest {
+declare module '@fastify/jwt' {
+  interface FastifyJWT {
     user: {
       sub: string;
       email: string;
